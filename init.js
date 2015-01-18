83171bd50ed9ce4be66c794b4d1f7795e65d7976
Naoki Rinmous <sukareki@gmail.com>
2015-01-18 23:31:32 +0800

    updated header

--- init.js
+++ init.js
@@ -1,3 +1,9 @@
+/*
+    MUGer's Almanac / 音游玩家老黄历
+
+    Copyright (c) 2015 Naoki Rinmous, published under MIT license.
+*/
+
 function nextScript(url) {
     var script = document.createElement('script');
     script.setAttribute('src', url + '?r=' + Math.random());
